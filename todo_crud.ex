2a395169d92895a23945beb6181589f741c0fe4d
Curtis Schlak <realistschuckle@gmail.com>
2015-09-18 21:18:43 -0500

    More work in the data abstraction chapter.

---
+++ todo_crud.ex
@@ -0,0 +1,46 @@
+defmodule TodoList do
+	defstruct auto_id: 1, entries: HashDict.new
+
+	def new(entries \\ []) do
+		Enum.reduce(entries, %TodoList{}, &add_entry(&2, &1))
+	end
+
+	def add_entry(%TodoList{entries: entries, auto_id: id} = list, entry) do
+		entry = Map.put(entry, :id, id)
+		new_entries = HashDict.put(entries, id, entry)
+		%TodoList{list | entries: new_entries, auto_id: id + 1}
+	end
+
+	def entries(%TodoList{entries: entries}, date) do
+		entries
+		|> Stream.filter(fn({_, entry}) ->
+			entry.date == date
+		end)						 
+		|> Enum.map(fn({_, entry}) -> entry end)
+	end
+
+	def update_entry(todo_list, %{} = new_entry) do
+		update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
+	end
+
+	def update_entry(%TodoList{entries: entries} = list, entry_id, updater) do
+		case entries[entry_id] do
+			nil -> list
+
+			old_entry ->
+				old_entry_id = old_entry.id
+				new_entry = %{id: ^old_entry_id} = updater.(old_entry)
+				new_entries = HashDict.put(entries, new_entry.id, new_entry)
+				%TodoList{list | entries: new_entries}
+		end
+	end
+
+	def delete_entry(%TodoList{entries: entries} = list, entry_id) do
+		new_entries = entries
+		|> Stream.filter(fn({_, entry}) ->
+			entry.id != entry_id
+		end)
+		|> Enum.map(fn({_, entry}) -> entry end)
+		%TodoList{list | entries: new_entries}
+	end
+end
