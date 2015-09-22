601bba9d773a130138d4f7d064db93a5459bcc4e
Curtis Schlak <realistschuckle@gmail.com>
2015-09-21 18:02:00 -0700

    Add key value store based on GenServer. Move todo_server to _own_rolled.

---
+++ key_value_store.ex
@@ -0,0 +1,28 @@
+defmodule KeyValueStore do
+	use GenServer
+
+	def start do
+		{:ok, pid} = GenServer.start(KeyValueStore, nil)
+		pid
+	end
+
+	def put(pid, key, value) do
+		GenServer.cast(pid, {:put, key, value})
+	end
+
+	def get(pid, key) do
+		GenServer.call(pid, {:get, key})
+	end
+	
+	def init(_) do
+		{:ok, HashDict.new}
+	end
+
+	def handle_cast({:put, key, value}, state) do
+		{:noreply, HashDict.put(state, key, value)}
+	end
+
+	def handle_call({:get, key}, _, state) do
+		{:reply, HashDict.get(state, key), state}
+	end
+end
