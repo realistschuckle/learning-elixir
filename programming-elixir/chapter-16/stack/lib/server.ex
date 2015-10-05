b96c762c13f6745d51de183748e0aef0bf0c6c44
Curtis Schlak <realistschuckle@gmail.com>
2015-10-05 13:45:20 -0500

    Programming Elixir: adding API calls to server.

--- programming-elixir/chapter-16/stack/lib/server.ex
+++ programming-elixir/chapter-16/stack/lib/server.ex
@@ -1,6 +1,14 @@
 defmodule Stack.Server do
 	use GenServer
 
+	def pop do
+		GenServer.call(__MODULE__, :pop)
+	end
+
+	def push(value) do
+		GenServer.cast(__MODULE__, {:push, value})
+	end
+
 	def start_link(state \\ []) do
 		GenServer.start_link(__MODULE__, state, name: __MODULE__)
 	end
