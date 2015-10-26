6c5f8a40be460a20b1a1a4cf869ea70902472419
Curtis Schlak <realistschuckle@gmail.com>
2015-10-25 19:12:37 -0500

    Authentication now working. Neat-o.

--- programming-phoenix/rumbl/web/controllers/user_controller.ex
+++ programming-phoenix/rumbl/web/controllers/user_controller.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.UserController do
 	use Rumbl.Web, :controller
 
+	plug :authenticate when action in [:index, :show]
+
 	def index(conn, _params) do
 		conn
 		|> render("index.html", users: Repo.all(Rumbl.User))
@@ -19,11 +21,12 @@ defmodule Rumbl.UserController do
 	end
 
 	def create(conn, %{"user" => user_params}) do
-		changeset = Rumbl.User.changeset(%Rumbl.User{}, user_params)
+		changeset = Rumbl.User.registration_changeset(%Rumbl.User{}, user_params)
 
 		case Repo.insert(changeset) do
 			{:ok, user} ->
 				conn
+				|> Rumbl.Auth.login(user)
 				|> put_flash(:info, "#{user.name} created!")
 				|> redirect(to: user_path(conn, :index))
 			{:error, changeset} ->
@@ -31,4 +34,19 @@ defmodule Rumbl.UserController do
 				|> render("new.html", changeset: changeset)
 		end
 	end
+
+	defp authenticate(conn, _) do
+		conn.assigns.current_user
+		|> authenticate_on_user(conn)
+	end
+
+	defp authenticate_on_user(nil, conn) do
+		conn
+		|> put_flash(:error, "You must be logged in to access that page.")
+		|> redirect(to: page_path(conn, :index))
+		|> halt
+	end
+
+	defp authenticate_on_user(_, conn),
+		do: conn
 end
--- programming-phoenix/rumbl/web/models/user.ex
+++ programming-phoenix/rumbl/web/models/user.ex
@@ -1,6 +1,8 @@
 defmodule Rumbl.User do
 	use Rumbl.Web, :model
 
+	alias Ecto.Changeset, as: CS
+
 	schema "users" do
 		field :name, :string
 		field :username, :string
@@ -15,4 +17,19 @@ defmodule Rumbl.User do
 		|> cast(params, ~w(name username), [])
 		|> validate_length(:username, min: 1, max: 20)
 	end
+
+	def registration_changeset(model, params \\ :empty) do
+		model
+		|> changeset(params)
+		|> cast(params, ~w(password), [])
+		|> validate_length(:password, min: 6, max: 100)
+		|> hash_password
+	end
+
+	defp hash_password(%CS{valid?: true, changes: %{password: pass}} = changeset) do
+		put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+	end
+
+	defp hash_password(changeset),
+		do: changeset
 end
